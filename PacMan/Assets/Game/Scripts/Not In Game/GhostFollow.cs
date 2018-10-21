01d7f563816a47d8d760430830c7b28280b9a936
Chris <chrisaconquet@hotmail.com>
2018-10-21 21:05:24 +0100

    PacMan Spel

---
+++ PacMan/Assets/Game/Scripts/Not In Game/GhostFollow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostFollow : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+    
+
+
+	void Start () {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+        //GhostFollow the player
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	
+	void Update () {
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f) {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else {
+                waitTime -= Time.deltaTime;
+            }
+
+        }
+
+	}//End Update
+
+
+    private void OnTriggerEnter2D(Collider2D collision){
+
+        //Kill player on contact
+        if (collision.gameObject.tag == "Player") {
+            Destroy(collision.gameObject);
+        }
+
+    }
+
+
+
+} //End Class
+
+
---
+++ PacMan/Assets/Game/Scripts/Saw.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour {
+
+    private int r;
+
+    public float min ;
+    public float max ;
+    
+    // Use this for initialization
+    void Start () {
+        min = transform.position.x;
+        max = transform.position.x + 3;
+        r = Random.Range(2, 10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * r, max - min) + min, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //Kill player on contact
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            print("You are a fag");
+        }
+
+    }
+
+}
